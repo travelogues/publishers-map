a54ae51b11f14c93b97ca97e9f6bfaf3e96ec255
Rainer Simon <aboutgeo@gmail.com>
2021-01-18 10:51:13 +0100

    Added a million RegEx cleaning rules

--- utils/get_distinct_places.js
+++ utils/get_distinct_places.js
@@ -1,7 +1,5 @@
-const { SourceMap } = require('module');
 const fs = require('fs');
 const Papa = require('papaparse');
-const file = 
 
 SOURCE_FILES = [
   '../../intertextuality/metadata/TravelogueD16_2020-06-09.csv',
@@ -10,7 +8,29 @@ SOURCE_FILES = [
   '../../intertextuality/metadata/TravelogueD19_1800-1820_Orient_2020-11-12.csv'
 ];
 
-const Verlagsorte = new Set();
+/**
+ * Normalizes a placenamen, fixing the most common patterns in the data
+ */
+const normalize = str => {
+  // Various variations of 'gedruckt (und verlegt) in'
+  str = str.replace(/[Gg]e[dt]ruckh?[t/]?( vnn?d verlegt)?( z[uů]|in)?/g, '').trim();
+  str = str.replace(/verlegt/g, '').trim();
+
+  // Various variations of 'In'
+  str = str.replace(/([Ji]nn? |[Zz]u )/g, '').trim();
+
+  // Enclosing square brackets
+  if (/^\[(.*?)\]?/g.test(str)) {
+    str = str.replace(/^\[/g, '');
+    str = str.replace(/\]$/g, '');
+  }
+
+  // Royal fluff :-)
+  str = str.replace(/(der Churfürstlichen Stadt|der Fürstl: Statt|der [kK][ae][yi]serlichen Stat?t)/g, '');
+  str = str.replace(/(der K[ae]yserliche\[?n\]? (Reychs Statt|Freystat))/g, '');
+
+  return str;
+}
 
 Promise.all(SOURCE_FILES.map(file =>
   new Promise((complete, error) =>
@@ -19,16 +39,25 @@ Promise.all(SOURCE_FILES.map(file =>
     })
   )
 )).then(results => {
+  const verlagsorte = new Set();
+
   results.forEach(result => {
     const placeColumn = result.data.map(obj => obj.Verlagsort.trim());
     placeColumn.forEach(colValue => {
-      const places = colValue.split('und').map(str => str.trim());
-      places.forEach(p => Verlagsorte.add(p));
+      const places = colValue.split(/und|;/i).map(str => str.trim());
+      places.forEach(p => verlagsorte.add(normalize(p)));
     });
   });
 
-  console.log(Verlagsorte);
-}).catch(err => console.log(err));
+  const sorted = Array.from(verlagsorte);
+  sorted.sort();
+  return sorted;
+}).catch(err => 
+  console.log(err)
+).then(places => {
+  places.forEach(p => console.log(p));
+});
+
 
 // Step 1: load source CSV files
 
