4fa200a211504d7e82f76a70e2bf970e08d04edb
Rainer Simon <aboutgeo@gmail.com>
2021-01-18 17:09:12 +0100

    (Almost) complete GeoJSON generation

--- utils/build_map_data.js
+++ utils/build_map_data.js
@@ -15,6 +15,15 @@ METADATA = [
 
 GEOLOCATIONS = '../data/unique_places_geocoded.csv';
 
+const parsePlaces = val => {
+  // Multiple places are separated by -. (convention)
+  const tokens = val.split('-.').map(str => str.trim());
+
+  // Each places is (usually "placename ; GND-ID")
+  return tokens.map(token => token.split(';')[0].trim())
+    .filter(token => token.length > 0);
+}
+
 // Step 1 - load data into mem
 const fLoadGeoLocations = new Promise((complete, error) =>
   Papa.parse(fs.createReadStream(GEOLOCATIONS), {
@@ -29,13 +38,78 @@ const fLoadMetadata = METADATA.map(f => new Promise((complete, error) =>
 ));
 
 Promise.all([ fLoadGeoLocations, ...fLoadMetadata ]).then(result => {
-  console.log(result);
+  const geolocations = result[0].data;
+  
+  const records = [ 
+    ...result[1].data, 
+    ...result[2].data, 
+    ...result[3].data, 
+    ...result[4].data ];
+
+  // Step 2 - build an index of the metadata records by placename
+  const recordIndex = {};
+  
+  records.forEach(record => {
+    const id = record['Systemnummer'];
+
+    const year = parseInt(record['Erscheinungsjahr Sortierform']);
+    
+    const markers = Array.from(new Set(
+      record['Marker'].split(/[\s;]/g)
+        .filter(token => token.length > 0)
+    ));
+
+    const publicationPlaces = parsePlaces(record['Verlagsort normiert ; GND-ID']);
+    const printingPlaces = parsePlaces(record['Druckort normiert ; GND-ID']);
+
+    const distinctPlaces = Array.from(new Set([ ...publicationPlaces, ...printingPlaces ]));
+
+    distinctPlaces.forEach(placename => {
+      const tags = [];
+
+      if (publicationPlaces.includes(placename))
+        tags.push('publication_place');
+
+      if (printingPlaces.includes(placename))
+        tags.push('printing_place');
+
+      const simplifiedRecord = {
+        id, year, markers, tags, all_places: distinctPlaces
+      };
+
+      const existing = recordIndex[placename];
+      if (existing) { 
+        existing.push(simplifiedRecord);
+      } else {
+        recordIndex[placename] = [ simplifiedRecord ];
+      }
+    });
+  });
+
+  // Step 3 - create GeoJSON-compatible data file
+  const mapData = {
+    type: 'FeatureCollection',
+    features: []
+  }
+
+  for (const [key, value] of Object.entries(recordIndex)) {
+    const place = geolocations.find(g => g.Ort === key.trim());
+
+    mapData.features.push({
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [ place.Lon, place.Lat ]
+      },
+      properties: { 
+        placename: key,
+        geonames_uri: place.GeoNames,
+        num_works: value.length
+      }
+    });
+  }
+
+  // Step 4 - write results
+  console.log(mapData);
+
 }).catch(error => console.log(error));
-
-// (Step 2 - build an index of the metadata records by place) - probably not even necessary, because our dataset is so small
-
-// Step 3 - for each place, get metadata records. Reduce them to a tuple Systemnr/year/tags ('printing_place', 'publishing_place')
-
-// Step 4 - output result file: list of places, each linked to the list of tuples
-
-// Let's us filter by time and aggregate counts for each place on the fly, so we can do proportional symbol mapping
