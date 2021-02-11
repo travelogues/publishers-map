277c19c686b6acedc2f84a6e6980ec5737f7f6ae
Rainer Simon <aboutgeo@gmail.com>
2021-02-11 10:38:40 +0100

    Basic map rendering

--- webmap/src/map/Map.jsx
+++ webmap/src/map/Map.jsx
@@ -1,21 +1,50 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import bbox from '@turf/bbox';
+import { MapConsumer, MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import useSWR from 'swr';
 
 import 'leaflet/dist/leaflet.css';
 
 import './Map.scss';
 
+const DEFAULT_BOUNDS = [
+  [ 45, 5 ],
+  [ 56, 20 ]
+];
+
+const getBounds = geojson => {
+  const bounds = bbox(geojson);
+  return [
+    bounds.slice(0, 2).slice().reverse(),
+    bounds.slice(2).slice().reverse()
+  ];
+}
+
 const Map = props => {
 
-  const position = [51.505, -0.09]
+  const { data } = useSWR('/map.json', url => fetch(url).then(r => r.json()));
+
+  const points = data?.features.filter(f => f?.geometry.type === 'Point');
 
   return (
-    <MapContainer center={position} zoom={13}>
+    <MapContainer bounds={DEFAULT_BOUNDS}>
+      <MapConsumer>
+        { map => {
+          const bounds = data ? getBounds(data) : DEFAULT_BOUNDS;
+          map.fitBounds(bounds);
+          return null;
+        }}
+      </MapConsumer>
+
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+
+      { points && points.map((f, idx) =>
+        <Marker key={idx} position={f.geometry.coordinates.slice().reverse()}>
+          <Popup>
+            <a href={f.properties.geonames_uri}>{f.properties.placename}</a> <br/>
+            {f.properties.num_works} works
+          </Popup>
+        </Marker>
+      )}
     </MapContainer>
   )
 
